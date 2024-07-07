2f9bf48a1ff56875ec310f919f0408f53629f51e
Chinthaka Abeyrathne <dilhanchinthaka99@gmail.com>
2024-07-08 01:30:54 +0530

    player UI is completed
    player UI is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Player from './components/Player'
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <div className='h-[90%] flex'>
         <Sidebar/>
       </div>
+      <Player/>
     </div>
   )
 }
