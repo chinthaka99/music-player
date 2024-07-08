2be3b7d566c57933a28ab36cd25cdc59b041b356
Chinthaka Abeyrathne <dilhanchinthaka99@gmail.com>
2024-07-08 19:53:19 +0530

    Complete the UI for DisplayItem and SongItems
    Complete the UI for DisplayItem and SongItems

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { albumsData } from '../assets/assets'
+import AlbemItem from './AlbemItem'
+import { songsData } from '../assets/assets'
+import SongsItem from './SongsItem'
 
 const DisplayHome = () => {
   return (
     <>
         <Navbar />
+        <div className='mb-4'>
+          <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+          <div className='flex overflow-auto'>
+              {albumsData.map((item,index)=>(<AlbemItem key={index} name={item.name} desc={item.desc}  id={item.id} image={item.image} />))}
+          </div> 
+        </div>
+
+        <div className='mb-4'>
+          <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hit</h1>
+          <div className='flex overflow-auto'>
+              {songsData.map((item, index)=>(<SongsItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+          </div> 
+        </div>
     </>
   )
 }
