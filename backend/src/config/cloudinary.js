fc890770b5fd3995d7289d524a71b2d2a573276d
Chinthaka Abeyrathne <dilhanchinthaka99@gmail.com>
2024-07-13 11:57:36 +0530

    configure the image and song uploading part to the database
    configure the cloudinary to upload images and mp3 files to the database

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -2,9 +2,9 @@ import {v2 as cloudinary} from 'cloudinary'
 
 const connectCloudinary = async ()=>{
     await cloudinary.config({
-        cloud_name:process.env.CLOUDINARY_NAME,
-        api_key:process.env.CLOUDINARY_API_KEY,
-        secret_key:process.env.CLOUDINARY_SECRET_KEY
+        cloud_name: process.env.CLOUDINARY_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_SECRET,
     })
 }
 
--- backend/src/controllers/songController.js
+++ backend/src/controllers/songController.js
@@ -1,7 +1,25 @@
+import {v2 as cloudinary} from 'cloudinary'
+import songModel from '../models/songModel.js';
 
 
 const addSong = async (req, res) => {
+    try {
+        const name = req.body.name;
+        const desc = req.body.desc;
+        const album = req.body.album;
+        const audioFile = req.files.audio[0];
+        const imageFile = req.files.image[0];
 
+        // when upload to cloudinary it will generate a one response and it is stored in the 'audioUpload' variable
+        const audioUpload = await cloudinary.uploader.upload(audioFile.path, {resource_type:"video"});
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:"image"});
+
+        console.log(name,desc,album,audioUpload,imageUpload);
+
+
+    } catch (error) {
+        
+    }
 }
 
 const listSong = async (req, res) => {
