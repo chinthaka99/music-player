853a6e151828cdcb71816fe2db0ec17da97755dc
Chinthaka Abeyrathne <dilhanchinthaka99@gmail.com>
2024-07-16 01:14:38 +0530

    complete the project
    complete the project

--- frontend/src/components/DisplayHome.jsx
+++ frontend/src/components/DisplayHome.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
 import AlbemItem from './AlbemItem'
-import { songsData } from '../assets/assets'
 import SongsItem from './SongsItem'
+import { useContext } from 'react'
+import { PlayerContext } from '../context/PlayerContext'
 
 const DisplayHome = () => {
+
+  const {songsData, albumsData} = useContext(PlayerContext);
+
   return (
     <>
         <Navbar />
         <div className='mb-4'>
           <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
           <div className='flex overflow-auto'>
-              {albumsData.map((item,index)=>(<AlbemItem key={index} name={item.name} desc={item.desc}  id={item.id} image={item.image} />))}
+              {albumsData.map((item,index)=>(<AlbemItem key={index} name={item.name} desc={item.desc}  id={item._id} image={item.image} />))}
           </div> 
         </div>
 
         <div className='mb-4'>
           <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hit</h1>
           <div className='flex overflow-auto'>
-              {songsData.map((item, index)=>(<SongsItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image}/>))}
+              {songsData.map((item, index)=>(<SongsItem key={index} name={item.name} desc={item.desc} id={item._id} image={item.image}/>))}
           </div> 
         </div>
     </>
