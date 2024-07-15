6b8776cd3ad3613732a3145b9ea9cd448b4fcc26
Chinthaka Abeyrathne <dilhanchinthaka99@gmail.com>
2024-07-15 23:15:33 +0530

    add functionality of the admin pannel to list all albums and delete albums from the list
    add functionality of the admin pannel to list all albums and delete albums from the list

--- backend/src/controllers/albumController.js
+++ backend/src/controllers/albumController.js
@@ -33,7 +33,7 @@ const addAlbum = async(req,res) => {
 const listAlbum = async(req,res) => {
     try {
         const allAlbum = await albumModel.find({});
-        res.json({success:true, album:allAlbum});
+        res.json({success:true, albums:allAlbum});
         
     } catch (error) {
         res.json({success:false})
